db46edc47a7df22051104a968e1b373f0f8b196f
Nathan Hocquette <nathan.hocquette@free.fr>
2022-11-20 17:29:30 +0100

    Removed interface because deprecated and new MFC interface incomming

--- SecureFolderInterface/ExitCodes.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define EXIT_SUCCESS 0
-#define EXIT_NO_WINDOW 1
-
-static int LastUnhandledError = EXIT_SUCCESS;
--- SecureFolderInterface/OperationnalHandles.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../SecureFolderCore/SecureFolderCore.h"
-#include "InputHandles.h"
-#include <shlobj_core.h>
-#include <Windows.h>
-
-bool readyToClose();
-void startOP(HWND hwnd, HINSTANCE hInstance);
